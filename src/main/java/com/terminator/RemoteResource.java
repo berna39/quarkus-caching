72383a00f74cd3f4873318475f46f0fcdbd5c662
berna39 <kalemajoseph39@gmail.com>
2023-11-11 23:05:19 +0100

    :sparkles: caching remote response

--- src/main/java/com/terminator/RemoteResource.java
+++ src/main/java/com/terminator/RemoteResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import io.quarkus.cache.CacheResult;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -13,9 +14,11 @@ import jakarta.ws.rs.PathParam;
 public interface RemoteResource {
     
     @GET
+    @CacheResult(cacheName = "cache-todos")
     List<Todo> getTodos();
 
     @GET
     @Path("/{id}")
+    @CacheResult(cacheName = "cache-todos-with-path")
     Todo getDoto(@PathParam("id") Integer id);
 }
