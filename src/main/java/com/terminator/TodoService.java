9cc473cb2b09deae1c7633f41681360809526a93
berna39 <kalemajoseph39@gmail.com>
2023-11-11 23:19:42 +0100

    :fix: error handling

--- src/main/java/com/terminator/TodoService.java
+++ src/main/java/com/terminator/TodoService.java
@@ -1,8 +1,10 @@
 package com.terminator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
 
 import io.quarkus.cache.CacheInvalidateAll;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -10,17 +12,31 @@ import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class TodoService {
+
+    private Logger logger;
     
     @Inject
     @RestClient
     RemoteResource remoteResource;
 
     public List<Todo> getTodos() {
-        return remoteResource.getTodos();
+        try {
+            return remoteResource.getTodos();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        return new ArrayList<>();
     }
 
     public Todo getTodo(Integer id) {
-        return remoteResource.getDoto(id);
+        try {
+            return remoteResource.getDoto(id);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        return null;
     }
 
     @CacheInvalidateAll(cacheName = "cache-todos")
